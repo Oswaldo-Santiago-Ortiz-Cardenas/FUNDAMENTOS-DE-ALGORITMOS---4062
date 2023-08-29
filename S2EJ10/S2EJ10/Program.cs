ce1586b17bf8cf6d2e3bd6b23f61374017856aa5
Oswaldo Santiago Ortiz Cardenas <143229490+Oswaldo-Santiago-Ortiz-Cardenas@users.noreply.github.com>
2023-08-28 23:17:16 -0500

    Add files via upload

---
+++ S2EJ10/S2EJ10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Programm
+{
+    public static void Main(string[] args)
+    {
+        double cons, desc, imp, T;
+        Console.WriteLine("Ingrese el costo de su consumo");
+        cons = double.Parse(Console.ReadLine());
+        if (cons <= 100.00)
+        {
+            imp = cons * 0.18;
+            desc = cons * 0.10;
+            T = cons - desc;
+            Console.WriteLine("Recibirá un descuento de 10% equivalente a " + desc + " soles");
+            Console.WriteLine("Se le aplicó un impuesto de 18% equivalente a " + imp + " soles");
+            Console.WriteLine("El subtotal de lo que debe pagar es de " + cons + " soles");
+            Console.WriteLine("El importa a pagar es de " + T + " soles");
+        }
+        else
+        {
+            imp = cons * 0.18;
+            desc = cons * 0.20;
+            T = cons - desc;
+            Console.WriteLine("Recibirá un descuento de 10% equivalente a " + desc + " soles");
+            Console.WriteLine("Se le aplicó un impuesto de 18% equivalente a " + imp + " soles");
+            Console.WriteLine("El subtotal de lo que debe pagar es de " + cons + " soles");
+            Console.WriteLine("El importa a pagar es de " + T + " soles");
+        }
+        Console.ReadKey();
+    }
+}
